ac59961f7ed92ff76d543b999fc33ee3069b3baa
Stela Tasheva <stela.tasheva@gmail.com>
2020-01-30 13:46:59 +0200

    javascrop handlers added to top navigation. Form pages are switching regarding the top navigation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,6 +6,7 @@ for (const navBtn of nav) {
 
 function stepHandler(e) {
     currentClassHandler(e);
+    switchPageHandler(e);
 }
 
 function currentClassHandler(e) {
@@ -13,4 +14,20 @@ function currentClassHandler(e) {
 
     nav.forEach(el => el.classList = "");
     target.classList.toggle('current')
+}
+
+function switchPageHandler(e){
+    const clickedId = e.target.id;
+    const allSections = document.querySelectorAll('.form-section');
+    
+    allSections.forEach(section => {
+        const sectionId = section.id;
+    
+        if(sectionId !== clickedId){
+            section.classList.add('hide');
+        }else {
+            section.classList.remove('hide');
+        }
+    });
+
 }
