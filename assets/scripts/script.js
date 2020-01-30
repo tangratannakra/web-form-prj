87eb2a247cf3007a96601a95e3378f0795562a31
Stela Tasheva <stela.tasheva@gmail.com>
2020-01-30 18:06:59 +0200

    reverting

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,7 @@
 const nav = document.querySelectorAll('.main-nav li');
+const nextBtn = document.querySelector('.next');
+
+nextBtn.addEventListener('click', nextHandler);
 
 for (const navBtn of nav) {
     navBtn.addEventListener('click', stepHandler);
@@ -6,6 +9,7 @@ for (const navBtn of nav) {
 
 function stepHandler(e) {
     currentClassHandler(e);
+    switchPageHandler(e);
 }
 
 function currentClassHandler(e) {
@@ -13,4 +17,37 @@ function currentClassHandler(e) {
 
     nav.forEach(el => el.classList = "");
     target.classList.toggle('current')
+}
+
+function switchPageHandler(e){
+    const clickedId = e.target.id;
+    const allSections = document.querySelectorAll('.form-section');
+    
+    allSections.forEach(section => {
+        const sectionId = section.id;
+
+        if(sectionId !== clickedId){
+            section.classList.add('hide');
+        }else {
+            section.classList.remove('hide');
+        }
+    });
+}
+
+function nextHandler(){
+    nav.forEach(navEl => {
+        //if (navEl.classList == "current"){
+        //     navEl.classList = '';
+        // }
+        // else{
+            navEl.firstChild.id=='second-page'?navEl.classList='current':navEl.classList="";
+      //  }
+
+    })
+
+    const allSections = document.querySelectorAll('.form-section');
+    
+    allSections.forEach(section => {
+        section.id == 'second-page'? section.classList.remove('hide'): section.classList.add('hide');
+    });
 }
