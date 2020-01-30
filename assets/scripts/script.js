72880345710b5dbad43758dbb39e76e2e8c3d6be
Stela Tasheva <stela.tasheva@gmail.com>
2020-01-30 17:52:52 +0200

    nextbtn functionallity added. changes nav statate and active section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,7 @@
 const nav = document.querySelectorAll('.main-nav li');
+const nextBtn = document.querySelector('.next');
+
+nextBtn.addEventListener('click', nextHandler);
 
 for (const navBtn of nav) {
     navBtn.addEventListener('click', stepHandler);
@@ -22,12 +25,29 @@ function switchPageHandler(e){
     
     allSections.forEach(section => {
         const sectionId = section.id;
-    
+
         if(sectionId !== clickedId){
             section.classList.add('hide');
         }else {
             section.classList.remove('hide');
         }
     });
+}
 
+function nextHandler(){
+    nav.forEach(navEl => {
+        //if (navEl.classList == "current"){
+        //     navEl.classList = '';
+        // }
+        // else{
+            navEl.firstChild.id=='second-page'?navEl.classList='current':navEl.classList="";
+      //  }
+
+    })
+
+    const allSections = document.querySelectorAll('.form-section');
+    
+    allSections.forEach(section => {
+        section.id == 'second-page'? section.classList.remove('hide'): section.classList.add('hide');
+    });
 }
